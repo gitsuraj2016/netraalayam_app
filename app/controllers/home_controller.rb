0cc89c8d38e65364b6c2aab2d3378de0459076cb
gitsuraj2016 <suraj.k1989@gmail.com>
2019-02-23 09:04:37 +0530

    Update home_controller.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,16 +8,5 @@ class HomeController < ApplicationController
      @custs = Store.where(:id=> current_user.store_id).all
   end
 
-  # def create_user
-  # 	  puts ">>>>>>>>>>#{params[:abc][:name]}"
-  # 	  puts ">>>>>>>>>>#{params[:abc][:password]}"
-  # 	  uu = User.new
-  # 	  uu.name = params[:abc][:name]
-  # 	  uu.email  = "abc@gmail.com"
-  # 	  uu.password = params[:abc][:password]
-  # 	  uu.save!
-  # 	  # User.create([{ name: 'lalit', email: 'lalit@gmail.com', password: "11111111", remember_created_at: nil }])
-
-  # 	  redirect_to :root 
-  # end
+  
 end
